61e59cd0eaec3fde9afc38f7c1d51df0d3ae03a8
Colin Watson <cjwatson@debian.org>
2001-05-04 02:37:16 +0000

    Improve dist target. Other cleanups.

--- src/man.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-pgm=`basename $0`
-edir=/usr/lib/man-db
-cmd="${edir}/${pgm} ${1+$@}"
-[ `id -u` = 0 ] || exec ${cmd}
-su nobody -c "/bin/true" && exec su nobody -c "$cmd"
-su -s /bin/true 2>/dev/null && exec su -s /bin/sh nobody -c "$cmd"
-exec su man -c "$cmd"
