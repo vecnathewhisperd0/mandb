821cf6a95a62b499d274fbe525779ce63b17d0de
Colin Watson <cjwatson@debian.org>
2009-08-17 18:33:24 +0100

    * src/tests/testlib.sh (init): New function.   (finish): Honour TEST_FAILURE_KEEP environment variable. * src/tests/manconv-1: Use init function. Reverse diff arguments so   that failure output makes more sense.

--- src/tests/testlib.sh
+++ src/tests/testlib.sh
@@ -1,5 +1,11 @@
 failures=0
 
+init () {
+	tmpdir="tmp-${0##*/}"
+	mkdir -p "$tmpdir" || exit $?
+	trap 'rm -rf "$tmpdir"' HUP INT QUIT TERM
+}
+
 expect_pass () {
 	ret=0
 	eval "$2" || ret=$?
@@ -14,9 +20,13 @@ expect_pass () {
 finish () {
 	case $failures in
 		0)
+			rm -rf "$tmpdir"
 			exit 0
 			;;
 		*)
+			if [ -z "$TEST_FAILURE_KEEP" ]; then
+				rm -rf "$tmpdir"
+			fi
 			exit 1
 			;;
 	esac
