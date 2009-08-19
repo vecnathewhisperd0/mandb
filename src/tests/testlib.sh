8199160b42b34c1a7c2b167aeb477a3ad81415a8
Colin Watson <cjwatson@debian.org>
2009-08-19 12:18:58 +0100

    * src/tests/testlib.sh (write_page): New function. * src/tests/lexgrog-1: New (fairly trivial) test. * src/tests/Makefile.am (TESTS): Add lexgrog-1.

--- src/tests/testlib.sh
+++ src/tests/testlib.sh
@@ -6,6 +6,31 @@ init () {
 	trap 'rm -rf "$tmpdir"' HUP INT QUIT TERM
 }
 
+# Arguments: name section path encoding compression_extension preprocessor_line name_line
+write_page () {
+	mkdir -p "${3%/*}"
+	>"$3.tmp1"
+	if [ "$6" ]; then
+		echo "'\\\" $6" >>"$3.tmp1"
+	fi
+	cat >>"$3.tmp1" <<EOF
+.TH $1 $2
+.SH NAME
+$7
+.SH DESCRIPTION
+test
+EOF
+	iconv -f UTF-8 -t "$4" <"$3.tmp1" >"$3.tmp2"
+	case $5 in
+		'')	cat ;;
+		gz|z)	gzip -9c ;;
+		Z)	compress -c ;;
+		bz2)	bzip2 -9c ;;
+		lzma)	lzma -9c ;;
+	esac <"$3.tmp2" >"$3"
+	rm -f "$3.tmp1" "$3.tmp2"
+}
+
 expect_pass () {
 	ret=0
 	eval "$2" || ret=$?
