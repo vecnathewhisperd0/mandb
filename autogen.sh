bd95b62bb153b1c67441ac2d4229ff2489bada1b
Colin Watson <cjwatson@debian.org>
2012-09-17 22:29:41 +0100

    Backport Gnulib commit 66712c23388e93e5c518ebc8515140fa0c807348 to stop assuming gets. * gnulib/gets.patch: New file. * autogen.sh: Apply gnulib/gets.patch. * NEWS: Document this.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 if type gnulib-tool >/dev/null 2>&1; then
 	autopoint -f
 	gnulib-tool --update >/dev/null
+	patch -s -p1 <gnulib/gets.patch
 	export AUTOPOINT=true
 fi
 export LIBTOOLIZE_OPTIONS=--quiet
