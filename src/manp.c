11bed8d98c379dc1070f1744e5340a48f10cd6b4
Will <cwr@netcom.co.uk>
2013-01-29 13:16:26 +0000

    * src/manp.c (add_to_dirlist): Increase key length to handle buried   binutils manual page paths.

--- src/manp.c
+++ src/manp.c
@@ -763,7 +763,7 @@ static void add_to_dirlist (FILE *config, int user)
 	char *bp;
 	char *buf = NULL;
 	size_t n = 0;
-	char key[50], cont[512];
+	char key[512], cont[512];
 	int val;
 	int c;
 
@@ -785,14 +785,15 @@ static void add_to_dirlist (FILE *config, int user)
 			goto next;	/* match any word starting with NO */
 		else if (sscanf (bp, "MANBIN %*s") == 1)
 			goto next;
-		else if (sscanf (bp, "MANDATORY_MANPATH %49s", key) == 1)
+		else if (sscanf (bp, "MANDATORY_MANPATH %511s", key) == 1)
 			add_mandatory (key);	
-		else if (sscanf (bp, "MANPATH_MAP %49s %511s", key, cont) == 2) 
+		else if (sscanf (bp, "MANPATH_MAP %511s %511s",
+			 key, cont) == 2) 
 			add_manpath_map (key, cont);
-		else if ((c = sscanf (bp, "MANDB_MAP %49s %511s",
+		else if ((c = sscanf (bp, "MANDB_MAP %511s %511s",
 				      key, cont)) > 0) 
 			add_mandb_map (key, cont, c, user);
-		else if ((c = sscanf (bp, "DEFINE %49s %511[^\n]",
+		else if ((c = sscanf (bp, "DEFINE %511s %511[^\n]",
 				      key, cont)) > 0)
 			add_def (key, cont, c, user);
 		else if (sscanf (bp, "SECTION %511[^\n]", cont) == 1)
