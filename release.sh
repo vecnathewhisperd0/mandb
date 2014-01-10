fd1c69d90198334d2b34a05e2128378d7c30596d
Colin Watson <cjwatson@debian.org>
2014-01-10 13:40:45 +0000

    Don't update man/po4a/po/ when nothing has changed
    * man/po4a/Makefile.am (DOMAIN): New variable.
    (EXTRA_DIST): Use $(DOMAIN).
    (STAGING): New variable.
    (PO4A_ARGS): Set new "podir" variable.
    (RUN_PO4A): New variable.
    (all-local): Abbreviate using RUN_PO4A.  Copy POT/PO files to
    $(STAGING) before running po4a, to avoid updating the master files.
    (update-po): Move to ...
    (update-po-real): ... here.  Abbreviate using RUN_PO4A.  Add
    --force, since this target is only called when we really want to
    update.  Copy POT/PO files to $(STAGING) before running po4a, and
    only copy them back if the POT file has changed by more than just
    the POT-Creation-Date.
    (update-po): New rule.  Call update-po-real if and only if srcdir =
    builddir.
    (clean-local): New rule.  Remove $(STAGING).
    * man/po4a/po4a.cfg (po4a_paths): Refer to $(podir) rather than
    $(srcdir)/po4a/po.
    * release.sh: Stop removing man/po4a/po/man-db-manpages.pot; this
    should no longer be necessary.

--- release.sh
+++ release.sh
@@ -7,9 +7,6 @@ set -e
 # Basic configure to get 'make distcheck'
 ./configure
 
-# Force regeneration with new version number
-rm -f man/po4a/po/man-db-manpages.pot
-
 # Make sure parsers are fresh
 if [ src/lexgrog.l -nt src/lexgrog.c ]; then
 	rm -f src/lexgrog.c
