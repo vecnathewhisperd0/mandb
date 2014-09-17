vecnathewhisperd0/mandb

commit 01e5a4febfc7b6cd53991455315ae7744c8f31dd

Partially rewrite building of manpath according to locale. The previous code was completely wrong: as well as handling duplicates rather oddly, it effectively handled LANGUAGE in reverse order (Debian bug #516133). * src/manp.c (add_nls_manpath): Rename to ...   (get_nls_manpath): ... this. Some minor adjustments. Make   manpathlist argument const and always return a freshly-allocated   string. Explicitly add the top-level directory to the string in   the case of English locales (although we still look for   subdirectories in case of things like en_GB.UTF-8); C and POSIX   already just return a copy of manpathlist.   (add_nls_manpaths): New function to iterate over a colon-separated   list of locales, concatenating the result of get_nls_manpath for   each. Stop worrying about deduplication at this point as we used   to; instead, we let create_pathlist sort that out.   (create_pathlist): Keep the first of any set of duplicates rather   than the last, in order to keep item order stable. * src/manp.h (add_nls_manpath): Remove prototype.   (add_nls_manpaths): Add prototype. * src/man.c (main), src/whatis.c (main): Call add_nls_manpaths   rather than add_nls_manpath, with appropriate adjustments. * src/man.c (main), src/whatis.c (main): Don'\''t set internal_locale   to the first component of multiple_locale. We will account for   this in search paths, but shouldn'\''t forget about internal_locale   entirely.

src/manp.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 1990, 1991 John W. Eaton.
  * Copyright (C) 1994, 1995 Graeme W. Wilford. (Wilf.)
- * Copyright (C) 2001, 2002, 2003, 2004, 2006, 2007, 2008 Colin Watson.
+ * Copyright (C) 2001, 2002, 2003, 2004, 2006, 2007, 2008, 2009 Colin Watson.
  *
  * This file is part of man-db.
  *
@@ -423,40 +423,32 @@ void free_locale_bits (struct locale_bits *bits)
 }
 
 
-char *add_nls_manpath (char *manpathlist, const char *locale)
+static char *get_nls_manpath (const char *manpathlist, const char *locale)
 {
-	char *manpath = NULL;
-	char *path;
 	struct locale_bits lbits;
-	char *omanpathlist;
-	char *manpathlist_ptr = manpathlist;
+	char *manpath = NULL;
+	char *manpathlist_copy, *path, *manpathlist_ptr;
 
-	debug ("add_nls_manpath(): processing %s\n", manpathlist);
-
-	if (locale == NULL || *locale == '\0')
-		return manpathlist;
 	unpack_locale_bits (locale, &lbits);
 	if (STREQ (lbits.language, "C") || STREQ (lbits.language, "POSIX")) {
 		free_locale_bits (&lbits);
-		return manpathlist;
+		return xstrdup (manpathlist);
 	}
 
-	omanpathlist = xstrdup (manpathlist);
-
+	manpathlist_copy = xstrdup (manpathlist);
+	manpathlist_ptr = manpathlist_copy;
 	for (path = strsep (&manpathlist_ptr, ":"); path;
 	     path = strsep (&manpathlist_ptr, ":")) {
 		DIR *mandir = opendir (path);
+		struct dirent *mandirent;
+
 		if (!mandir)
 			continue;
 
-		for (;;) {
-			struct dirent *mandirent = readdir (mandir);
+		while ((mandirent = readdir (mandir)) != NULL) {
 			const char *name;
 			struct locale_bits mbits;
 			char *fullpath;
-
-			if (!mandirent)
-				break;
 
 			name = mandirent->d_name;
 			if (STREQ (name, ".") || STREQ (name, ".."))
@@ -479,16 +471,90 @@ char *add_nls_manpath (char *manpathlist, const char *locale)
 			free_locale_bits (&mbits);
 			free (fullpath);
 		}
+
+		if (STREQ (lbits.language, "en"))
+			/* For English, we look in the subdirectories as
+			 * above just in case there's something like
+			 * en_GB.UTF-8, but it's more probable that English
+			 * manual pages reside at the top level.
+			 */
+			manpath = pathappend (manpath, path);
 	}
 
 	free_locale_bits (&lbits);
+	return manpath;
+}
 
-	/* After doing all the locale stuff we add the manpath to the *END*
-	 * so the locale dirs are checked first on each section */
-	manpath = pathappend (manpath, omanpathlist);
-	free (omanpathlist);
+char *add_nls_manpaths (char *manpathlist, const char *locales)
+{
+	char *manpath = NULL;
+	char *locales_copy, *tok, *locales_ptr;
+	char *locale_manpath;
 
-	free (manpathlist);
+	debug ("add_nls_manpaths(): processing %s\n", manpathlist);
+
+	if (locales == NULL || *locales == '\0')
+		return manpathlist;
+
+	/* For each locale, we iterate over the manpath and find appropriate
+	 * locale directories for each item. We then concatenate the results
+	 * for all locales. In other words, LANGUAGE=fr:de and
+	 * manpath=/usr/share/man:/usr/local/share/man could result in
+	 * something like this list:
+	 *
+	 *   /usr/share/man/fr
+	 *   /usr/local/share/man/fr
+	 *   /usr/share/man/de
+	 *   /usr/local/share/man/de
+	 *   /usr/share/man
+	 *   /usr/local/share/man
+	 *
+	 * This assumes that it's more important to have documentation in
+	 * the preferred language than to have documentation for the correct
+	 * object (in the case where there are different versions of a
+	 * program in different hierarchies, for example). It is not
+	 * entirely obvious that this is the right assumption, but on the
+	 * other hand the other choice is not entirely obvious either. We
+	 * tie-break on "we've always done it this way", and people can use
+	 * 'man -a' or whatever in the occasional case where we get it
+	 * wrong.
+	 *
+	 * We go to no special effort to de-duplicate directories here.
+	 * create_pathlist will sort it out later; note that it preserves
+	 * order in that it keeps the first of any duplicate set in its
+	 * original position.
+	 */
+
+	locales_copy = xstrdup (locales);
+	locales_ptr = locales_copy;
+	for (tok = strsep (&locales_ptr, ":"); tok;
+	     tok = strsep (&locales_ptr, ":")) {
+		if (!*tok)	/* ignore empty fields */
+			continue;
+		debug ("checking for locale %s\n", tok);
+
+		locale_manpath = get_nls_manpath (manpathlist, tok);
+		if (locale_manpath) {
+			if (manpath)
+				manpath = appendstr (manpath, ":",
+						     locale_manpath, NULL);
+			else
+				manpath = xstrdup (locale_manpath);
+			free (locale_manpath);
+		}
+	}
+
+	/* Always try untranslated pages as a last resort. */
+	locale_manpath = get_nls_manpath (manpathlist, "C");
+	if (locale_manpath) {
+		if (manpath)
+			manpath = appendstr (manpath, ":",
+					     locale_manpath, NULL);
+		else
+			manpath = xstrdup (locale_manpath);
+		free (locale_manpath);
+	}
+
 	return manpath;
 }
 
@@ -1117,7 +1183,7 @@ void create_pathlist (const char *manp, char **mp)
 	/* Eliminate duplicates due to symlinks. */
 	mp = mphead;
 	while (*mp) {
-		char *target;
+		char *target, *oldmp = NULL;
 		char **dupcheck;
 		int found_dup = 0;
 
@@ -1125,15 +1191,21 @@ void create_pathlist (const char *manp, char **mp)
 		 * manpath?
 		 */
 		target = canonicalize_file_name (*mp);
-		if (!target) {
-			++mp;
-			continue;
+		if (target) {
+			oldmp = *mp;
+			*mp = target;
 		}
-		for (dupcheck = mphead; *dupcheck; ++dupcheck) {
-			if (mp == dupcheck || !STREQ (target, *dupcheck))
+		/* Only check up to the current list position, to keep item
+		 * order stable across deduplication.
+		 */
+		for (dupcheck = mphead; *dupcheck && dupcheck != mp;
+		     ++dupcheck) {
+			if (!STREQ (*mp, *dupcheck))
 				continue;
-			debug ("Removing duplicate manpath entry %s -> %s\n",
-			       *mp, target);
+			debug ("Removing duplicate manpath entry %s (%d) -> "
+			       "%s (%d)\n",
+			       oldmp, mp - mphead,
+			       *dupcheck, dupcheck - mphead);
 			free (*mp);
 			for (dupcheck = mp; *(dupcheck + 1); ++dupcheck)
 				*dupcheck = *(dupcheck + 1);
@@ -1141,9 +1213,24 @@ void create_pathlist (const char *manp, char **mp)
 			found_dup = 1;
 			break;
 		}
-		free (target);
+		if (oldmp)
+			free (oldmp);
 		if (!found_dup)
 			++mp;
+	}
+
+	if (debug_level) {
+		int first = 1;
+
+		debug ("final search path = ");
+		for (mp = mphead; *mp; ++mp) {
+			if (first) {
+				debug ("%s", *mp);
+				first = 0;
+			} else
+				debug (":%s", *mp);
+		}
+		debug ("\n");
 	}
 }
 

src/man.c
@@ -3,7 +3,8 @@
  *
  * Copyright (C) 1990, 1991 John W. Eaton.
  * Copyright (C) 1994, 1995 Graeme W. Wilford. (Wilf.)
- * Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 Colin Watson.
+ * Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
+ * Colin Watson.
  *
  * This file is part of man-db.
  *
@@ -903,14 +904,7 @@ int main (int argc, char *argv[])
 	if (internal_locale && strcmp (internal_locale, "C") &&
 	    strcmp (internal_locale, "POSIX"))
 		multiple_locale = getenv ("LANGUAGE");
-	if (multiple_locale && *multiple_locale) {
-		const char *colon = strchr (multiple_locale, ':');
-		internal_locale = colon ?
-			xstrndup (multiple_locale, colon - multiple_locale) :
-			xstrdup (multiple_locale);
-	} else
-		internal_locale = xstrdup (internal_locale ?
-					   internal_locale : "C");
+	internal_locale = xstrdup (internal_locale ? internal_locale : "C");
 
 /* export argv, it might be needed when invoking the vendor supplied browser */
 #if defined _AIX || defined __sgi
@@ -1044,24 +1038,29 @@ int main (int argc, char *argv[])
 	}
 
 	if (manp == NULL) {
-		manp = add_nls_manpath (get_manpath (alt_system_name),
-					internal_locale);
-		/* Handle multiple :-separated locales in LANGUAGE */
+		char *all_locales;
+
 		if (multiple_locale && *multiple_locale) {
-			char *localetok = xstrdup (multiple_locale), *tok;
-			char *localetok_ptr = localetok;
-			for (tok = strsep (&localetok_ptr, ":"); tok;
-			     tok = strsep (&localetok_ptr, ":")) {
-				if (!*tok)	/* ignore empty fields */
-					continue;
-				debug ("checking for locale %s\n", tok);
-				manp = add_nls_manpath (manp, tok);
-			}
+			if (internal_locale && *internal_locale)
+				all_locales = xasprintf ("%s:%s",
+							 multiple_locale,
+							 internal_locale);
+			else
+				all_locales = xstrdup (multiple_locale);
+		} else {
+			if (internal_locale && *internal_locale)
+				all_locales = xstrdup (internal_locale);
+			else
+				all_locales = NULL;
 		}
+
+		manp = add_nls_manpaths (get_manpath (alt_system_name),
+					 all_locales);
+		free (all_locales);
 	} else
 		free (get_manpath (NULL));
 
-	debug ("*manpath search path* = %s\n", manp);
+	debug ("manpath search path (with duplicates) = %s\n", manp);
 
 	create_pathlist (manp, manpathlist);
 

src/manp.h
@@ -33,7 +33,7 @@ struct locale_bits {
 extern char *cat_manpath (char *manp);
 extern void unpack_locale_bits (const char *locale, struct locale_bits *bits);
 extern void free_locale_bits (struct locale_bits *bits);
-extern char *add_nls_manpath (char *manpathlist, const char *locale);
+extern char *add_nls_manpaths (char *manpathlist, const char *locales);
 extern char *get_manpath (const char *systems);
 extern void create_pathlist (const char *manp, char **mp);
 extern void free_pathlist (char **mp);

src/whatis.c
@@ -2,7 +2,7 @@
  * whatis.c: search the index or whatis database(s) for words.
  *  
  * Copyright (C) 1994, 1995 Graeme W. Wilford. (Wilf.)
- * Copyright (C) 2001, 2002, 2003, 2004, 2006, 2007, 2008 Colin Watson.
+ * Copyright (C) 2001, 2002, 2003, 2004, 2006, 2007, 2008, 2009 Colin Watson.
  *
  * This file is part of man-db.
  *
@@ -55,6 +55,7 @@
 #include "argp.h"
 #include "dirname.h"
 #include "fnmatch.h"
+#include "xvasprintf.h"
 
 #include "manconfig.h"
 
@@ -700,14 +701,7 @@ int main (int argc, char *argv[])
 	if (internal_locale && strcmp (internal_locale, "C") &&
 	    strcmp (internal_locale, "POSIX"))
 		multiple_locale = getenv ("LANGUAGE");
-	if (multiple_locale && *multiple_locale) {
-		const char *colon = strchr (multiple_locale, ':');
-		internal_locale = colon ?
-			xstrndup (multiple_locale, colon - multiple_locale) :
-			xstrdup (multiple_locale);
-	} else
-		internal_locale = xstrdup (internal_locale ?
-					   internal_locale : "C");
+	internal_locale = xstrdup (internal_locale ? internal_locale : "C");
 
 	if (argp_parse (am_apropos ? &apropos_argp : &whatis_argp, argc, argv,
 			0, 0, 0))
@@ -734,20 +728,25 @@ int main (int argc, char *argv[])
 
 	/* sort out the internal manpath */
 	if (manp == NULL) {
-		manp = add_nls_manpath (get_manpath (alt_systems),
-					internal_locale);
-		/* Handle multiple :-separated locales in LANGUAGE */
+		char *all_locales;
+
 		if (multiple_locale && *multiple_locale) {
-			char *localetok = xstrdup (multiple_locale), *tok;
-			char *localetok_ptr = localetok;
-			for (tok = strsep (&localetok_ptr, ":"); tok;
-			     tok = strsep (&localetok_ptr, ":")) {
-				if (!*tok)	/* ignore empty fields */
-					continue;
-				debug ("checking for locale %s\n", tok);
-				manp = add_nls_manpath (manp, tok);
-			}
+			if (internal_locale && *internal_locale)
+				all_locales = xasprintf ("%s:%s",
+							 multiple_locale,
+							 internal_locale);
+			else
+				all_locales = xstrdup (multiple_locale);
+		} else {
+			if (internal_locale && *internal_locale)
+				all_locales = xstrdup (internal_locale);
+			else
+				all_locales = NULL;
 		}
+
+		manp = add_nls_manpaths (get_manpath (alt_systems),
+					 all_locales);
+		free (all_locales);
 	} else
 		free (get_manpath (NULL));
 

commit 09fa9faab70ed2f4c4c04cc1ad18a8aeac9b8e66

Don't store canonicalised versions of manpath elements
We still compare canonicalised versions, but don't store them.
Storing them results in looking up the wrong catpath in the case
where one of the configured MANDB_MAP entries is a symlink.

Broken by commit 01e5a4febfc7b6cd53991455315ae7744c8f31dd; fixes
Fedora bug #1043401.

* src/manp.c (create_pathlist): Compare canonicalised versions of
manpath elements, but don't store them.
* NEWS: Document this.

src/manp.c
@@ -1219,7 +1219,7 @@ void create_pathlist (const char *manp, char **mp)
 	/* Eliminate duplicates due to symlinks. */
 	mp = mphead;
 	while (*mp) {
-		char *target, *oldmp = NULL;
+		char *target;
 		char **dupcheck;
 		int found_dup = 0;
 
@@ -1227,20 +1227,25 @@ void create_pathlist (const char *manp, char **mp)
 		 * manpath?
 		 */
 		target = canonicalize_file_name (*mp);
-		if (target) {
-			oldmp = *mp;
-			*mp = target;
+		if (!target) {
+			++mp;
+			continue;
 		}
 		/* Only check up to the current list position, to keep item
 		 * order stable across deduplication.
 		 */
 		for (dupcheck = mphead; *dupcheck && dupcheck != mp;
 		     ++dupcheck) {
-			if (!STREQ (*mp, *dupcheck))
+			char *dupcheck_target = canonicalize_file_name
+				(*dupcheck);
+			if (!STREQ (target, dupcheck_target)) {
+				free (dupcheck_target);
 				continue;
+			}
+			free (dupcheck_target);
 			debug ("Removing duplicate manpath entry %s (%td) -> "
 			       "%s (%td)\n",
-			       oldmp, mp - mphead,
+			       *mp, mp - mphead,
 			       *dupcheck, dupcheck - mphead);
 			free (*mp);
 			for (dupcheck = mp; *(dupcheck + 1); ++dupcheck)
@@ -1249,8 +1254,7 @@ void create_pathlist (const char *manp, char **mp)
 			found_dup = 1;
 			break;
 		}
-		if (oldmp)
-			free (oldmp);
+		free (target);
 		if (!found_dup)
 			++mp;
 	}