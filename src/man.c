b225d9e76fbb0a6a4539c0992fba88c83f0bd37e
Colin Watson <cjwatson@debian.org>
2017-11-22 00:02:24 +0000

    Remove Easter egg entirely
    Six years is a reasonable shelf life for a joke, but I think its time
    has passed now.
    
    * src/man.c (main): Remove Easter egg.

--- src/man.c
+++ src/man.c
@@ -4125,13 +4125,6 @@ int main (int argc, char *argv[])
 			printf ("%s\n", manp);
 			exit (OK);
 		} else {
-			/* http://twitter.com/#!/marnanel/status/132280557190119424 */
-			time_t now = time (NULL);
-			struct tm *localnow = localtime (&now);
-			if (localnow &&
-			    localnow->tm_hour == 0 && localnow->tm_min == 30)
-				fprintf (stderr, "gimme gimme gimme\n");
-
 			free_cwd (&cwd);
 			free (internal_locale);
 			free (program_name);
