ecba2e037bcb33b7fd4f0caa7bc42bd05fefdfd6
Colin Watson <cjwatson@debian.org>
2018-03-17 01:06:05 +0000

    sandbox: Allow madvise
    Reported by Tobias Klausmann.
    
    * lib/sandbox.c (make_seccomp_filter): Allow madvise.
    * NEWS: Document this.

--- lib/sandbox.c
+++ lib/sandbox.c
@@ -476,6 +476,7 @@ scmp_filter_ctx make_seccomp_filter (int permissive)
 		SC_ALLOW_ARG_1 ("ioctl", SCMP_A1 (SCMP_CMP_EQ, TCGETS));
 		SC_ALLOW_ARG_1 ("ioctl", SCMP_A1 (SCMP_CMP_EQ, TIOCGWINSZ));
 	}
+	SC_ALLOW ("madvise");
 	SC_ALLOW ("mprotect");
 	SC_ALLOW ("mremap");
 	SC_ALLOW ("sync_file_range2");
