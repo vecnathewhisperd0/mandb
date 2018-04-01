5d0571d37309fb296f8fd444db4d7fdfeb63e725
Colin Watson <cjwatson@debian.org>
2018-04-01 10:58:39 +0100

    Fix locale_macros version check for groff RCs
    Thanks to Werner LEMBERG.
    
    * src/man.c (locale_macros): Tolerate groff release candidates.
    * NEWS: Document this.

--- src/man.c
+++ src/man.c
@@ -2136,15 +2136,20 @@ static void locale_macros (void *data)
 		/* If we're using groff >= 1.20.2 (for the 'file' warning
 		 * category):
 		 */
-		".if (\\n[.g] & ((\\n[.x] > 1) :"
+		".if \\n[.g] \\{\\\n"
+		".  ds Ystring \\n[.Y]\n"
+		".  while (\\B'\\*[Ystring]' = 0) .chop Ystring\n"
+		".  if ((\\n[.x] > 1) :"
 		" ((\\n[.x] == 1) & (\\n[.y] > 20)) :"
-		" ((\\n[.x] == 1) & (\\n[.y] == 20) & (\\n[.Y] >= 2)))) "
+		" ((\\n[.x] == 1) & (\\n[.y] == 20) & (\\*[Ystring] >= 2))) "
 		"\\{\\\n"
 		/*   disable warnings of category 'file' */
-		".  warn (\\n[.warn] -"
+		".    warn (\\n[.warn] -"
 		" (\\n[.warn] / 1048576 %% 2 * 1048576))\n"
 		/*   and load the appropriate per-locale macros */
-		".  mso %s.tmac\n"
+		".    mso %s.tmac\n"
+		".  \\}\n"
+		".  rm Ystring\n"
 		".\\}\n"
 		/* set the hyphenation language anyway, to make sure groff
 		 * only hyphenates languages it knows about
