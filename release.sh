ab419a6303da20f0c7d55ddf44b16711181dfa7e
Colin Watson <cjwatson@debian.org>
2018-11-09 21:33:34 +0000

    release.sh: Simplify Lex handling
    release.sh had a hack to ensure that flex-generated scanners are fresh
    in release tarballs.  This is more easily achieved by configuring with
    --enable-maintainer-mode, which causes Automake to enable the Lex
    rebuild rule.
    
    * release.sh: Configure with --enable-maintainer-mode; drop conditional
    removals of src/lexgrog.c and src/zsoelim.c.

--- release.sh
+++ release.sh
@@ -5,14 +5,6 @@ set -e
 ./bootstrap
 
 # Basic configure to get 'make distcheck'
-./configure
-
-# Make sure parsers are fresh
-if [ src/lexgrog.l -nt src/lexgrog.c ]; then
-	rm -f src/lexgrog.c
-fi
-if [ src/zsoelim.l -nt src/zsoelim.c ]; then
-	rm -f src/zsoelim.c
-fi
+./configure --enable-maintainer-mode
 
 make distcheck
